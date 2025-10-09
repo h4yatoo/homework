08501614d1c5103f43f722fd65f4b9ccbd23cabd
h4yatoo <balandin27072008@gmail.com>
2025-10-09 22:23:07 +0200

    dz

---
+++ 9.10.25.py
@@ -0,0 +1,58 @@
+
+#     Задача 1
+# a,b=map(int,input().split())
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+     # Задача 2
+# a,b=map(int,input().split())
+# if a>b and a%2==0:
+#     print('Первое больше и четное')
+# elif a>b and a%2==1:
+#     print('Первое больше и нечетное')
+# elif a>b and a%10==0:
+#     print('Первое больше и оканчивается на 0')
+# elif b>=a:
+#     print(b)
+
+   # Задача 3
+# a,b=map(int,input().split())
+# if a>0 and b>0:
+#     print(a+b)
+# elif a<0 or b<0:
+#     print(a*b)
+# elif a==0 and b==0:
+#     print('Оба ноль')
+
+    # Задача 4
+# a,b,c=map(int,input().split())
+# if a<b+c and b<a+c and c<a+b:
+#     print('можно')
+# else:
+#     print('нельзя')
+
+   # Задача 6
+# a, b, c = map(int,input().split())
+# D = b**2 - 4*a*c
+# if D < 0:
+#     print('Нет решений')
+# elif D == 0:
+#     x = -b / (2*a)
+#     print(x)
+# else:
+#     x1 = (-b - D**0.5) / (2*a)
+#     x2 = (-b + D**0.5) / (2*a)
+#     print(x1)
+#     print(x2)
+
+   # Задача 5
+# x1,y1 = map(int,input().split())
+# x2,y2 = map(int,input().split())
+# if (x1 == x2 + 2 or x1 == x2 - 2) and (y1 == y2 - 1 or y1 == y2 + 1) or (x1 == x2 + 1 or x1 == x2 - 1) and (y1 == y2 - 2 or y1 == y2 + 2):
+#     print('да')
+# else:
+#     print('нет')
+
+
