82dcc69d8cab7589fd8bc0d3565ad4f250b4e78d
h4yatoo <balandin27072008@gmail.com>
2025-10-15 17:49:48 +0200

    dz

---
+++ 15.10.2025.py
@@ -0,0 +1,50 @@
+# 1
+# a=int(input())
+# match a%5:
+#     case 0:
+#         print('true')
+#     case _:
+#          print('false')
+
+#2
+# a=int(input())
+# match a:
+#     case a if 10<=a<=20:
+#         print('да')
+#     case _:
+#         print('нет')
+
+#3
+# x=float(int(input()))
+# y=float(int(input()))
+# match (x,y):
+#     case (0,0):
+#         print('в центре графикаэ')
+#     case (0,_):
+#         print('на y')
+#     case (_, 0):
+#         print('на x')
+#     case f if x > 0 and y > 0:
+#         print("Первая четверть")
+#     case f if x < 0 and y > 0:
+#         print("Вторая четверть")
+#     case f if x < 0 and y < 0:
+#         print("Третья четверть")
+#     case f if x > 0 and y < 0:
+#         print("Четвёртая четверть")
+
+#4
+# a=input('+,-,*,/')
+# b=int(input())
+# c=int(input())
+# match a:
+#     case '+':
+#         print(b+c)
+#     case '-':
+#         print(b-c)
+#     case '*':
+#         print(b*c)
+#     case '/':
+#         print(b/c)
+
+
