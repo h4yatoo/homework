2e788444f2ecd2835bc829b8a2ee91deb10cd7ac
h4yatoo <balandin27072008@gmail.com>
2025-10-18 17:22:45 +0200

    dz

---
+++ 18.10.2025.py
@@ -0,0 +1,35 @@
+# 1
+# for i in range(1,19,2):
+#     print(i)
+
+# 2
+# n=int(input())
+# n=abs(n)
+# a=0
+# while n>0:
+#     a+=n%10
+#     n//=10
+# print(a)
+
+# 3
+# n=list(map(int,input().split()))
+# for i in n:
+#     if i>100:
+#         print(i)
+#         break
+# else:
+#     print('таких чисел нет')
+
+# 4
+# n=int(input())
+# for i in range(2,n):
+#     if n%i==0:
+#         print(i)
+#         break
+# else:
+#     print('число простое')
+
+# 5
+# a=[['*'],['**'],['***'],['****']]
+# for i in a:
+#     я не доделал
