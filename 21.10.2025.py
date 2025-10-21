432b05c98c563effaa6f9c64f9f441cc63eac71e
h4yatoo <balandin27072008@gmail.com>
2025-10-21 23:36:18 +0200

    dz

---
+++ 21.10.2025.py
@@ -0,0 +1,46 @@
+# #2
+# while True:
+#     a=input()
+#     if a=='yes':
+#         for i in range(1,5):
+#             for j in range((4-i),-1,-1):
+#                 print('*'*i+' '*j)
+#                 break
+#         break
+#     else:
+#         print('-----')
+
+#1
+# l='qwertyuiopasdfghjklzxcvbnm'
+# for perviy in l:
+#     for vtoroy in l:
+#         for tretiy in l:
+#             code=perviy+vtoroy+tretiy
+#             print(code)
+
+#3
+# a=float(input())
+# c=input()
+# b=float(input())
+# while True:
+#     if c=='+':
+#         print(a+b)
+#     elif c=='-':
+#         print(a-b)
+#     elif c=='*':
+#         print(a*b)
+#     elif c=='/':
+#         print(a/b)
+#     elif c=='**':
+#         print(a**b)
+#         break
+#     elif c == 'stop':
+#         break
+
+#4
+# a=input()
+# if a==a[::-1]:
+#     print('палидром')
+# else:
+#     print('не палидром')
+
