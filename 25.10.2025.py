0d71738e238501be7200f698fdfb72501eb97a08
h4yatoo <balandin27072008@gmail.com>
2025-10-25 16:31:07 +0200

    dz

---
+++ 25.10.2025.py
@@ -0,0 +1,25 @@
+#1
+# for i in range(100,9,-10):
+#     print(i)
+
+#2
+# while True:
+#     number = int(input("Enter a number: "))
+#     print(number)
+#     if number%5==0:
+#         print('число кратно 5')
+#         break
+
+#3
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i,'*',j, '=',i*j)
+
+#4
+# a=int(input())
+# b=int(input())
+# c=0
+# for i in range(a,b+1):
+#     c+=i**2
+#     print(c)
