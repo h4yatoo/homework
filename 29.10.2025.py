57fac8b182fdf0bda117af5d757ee60c9eb28111
h4yatoo <balandin27072008@gmail.com>
2025-10-29 14:44:52 +0200

    dz

---
+++ 29.10.2025.py
@@ -0,0 +1,31 @@
+# 1
+# a=input()
+# print(a[::-1])
+
+# 2
+# a=input()
+# print(a[0:2])
+
+# 3
+# a=input()
+# print(a[:5]+a[-5:])
+
+# 4
+# a=input()
+# print(len(a))
+
+# 5
+# a=input()
+# if 'o' in a:
+#     print(a.find('o'))
+# else:
+#     print('такого символа нет')
+
+# 6
+# a=input()
+# if 'o' in a:
+#     print(a.rfind('o'))
+# else:
+#     print('такого символа нет')
+
+
