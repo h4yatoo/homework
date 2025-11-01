618da1e729f795e2edf7851f0d1545a761a7fc88
h4yatoo <balandin27072008@gmail.com>
2025-11-01 16:07:33 +0200

    dz

---
+++ 1.11.2025.py
@@ -0,0 +1,27 @@
+# 1
+# a=input()
+# if a.endswith('ов'):
+#     print('da')
+# else:
+#     print('net')
+
+# 2
+# a=input()
+# if (c.isdigit() for c in a):
+#     print('da')
+# else:
+#     ('net')
+
+# 3
+# a=input()
+# b=a.split()
+# print(','.join(b))
+
+# 4
+# a=input()
+# b='aoiuyeAOIUYE'
+# c=''
+# for i in a:
+#     if i not in b:
+#         c+=i
+# print(c)
