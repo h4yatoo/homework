865d4244b53c2555a66edc51eafbfd6f809ef72f
h4yatoo <balandin27072008@gmail.com>
2025-11-08 14:35:53 +0200

    dz

---
+++ 08.11.2025.py
@@ -0,0 +1,18 @@
+# a=int(input())
+# n=1
+# for i in range(a):
+#     print(n)
+#     n/=-2
+
+# a=input()
+# b=input()
+# c=0
+# for i in a:
+#     if i==b:
+#         c+=1
+# print(c)
+
+# a=input()
+# for i in range(len(a)):
+#     print(i+1, a[i])
+
