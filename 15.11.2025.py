3ee934639d2b2078413da878c5468db274edccab
h4yatoo <balandin27072008@gmail.com>
2025-11-15 18:01:28 +0200

    dz

---
+++ 15.11.2025.py
@@ -0,0 +1,15 @@
+# set1 = {1, 2, 3}
+# set2 = {4, 5, 6}
+# print(set1.union(set2))
+
+# fruits = ["яблоко", "банан", "яблоко", "апельсин", "банан", "киви"]
+# fruitss=set(fruits)
+# print(len(fruitss))
+
+# a = {1, 2, 3, 4}
+# b={3, 4, 5, 6}
+# print(a.intersection(b))
+
+# s=('a','b','c')
+# a,b,c=s
+# print(b)
