2b27d44e9b66759c43c846ff1dc7d80aaef57124
h4yatoo <balandin27072008@gmail.com>
2025-11-22 17:51:11 +0200

    dz

---
+++ 22.11.2025.py
@@ -0,0 +1,12 @@
+# a=input()
+# s={}
+# for i in a:
+#     s[i]=s.get(i,0)+1
+# print(s)
+
+# a=tuple(map(int,input().split()))
+# s={}
+# for i in a:
+#     s[i]=s.get(i,0)+1
+# print(a)
+# print(s)
