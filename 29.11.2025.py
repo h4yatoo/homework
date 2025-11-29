6cf9bc1cb7b65a401e0af013825b5cda043e6fbc
h4yatoo <balandin27072008@gmail.com>
2025-11-29 17:50:11 +0200

    dz

---
+++ 29.11.2025.py
@@ -0,0 +1,22 @@
+# def d(n):
+#     a=1
+#     for i in range(1,n+1):
+#         a*=i
+#     return a
+# print(d(3))
+#
+# def ntd(n):
+#     a=str(n)
+#     b=[int(b) for b in a]
+#     return b
+# print(ntd(3))
+
+# def isp(n):
+#     if n<=1: return False
+#     if n==2: return True
+#     if n%2==0: return False
+#     for i in range(3,int(n**0.5)+1,2):
+#         if n%i==0: return False
+#     return True
+# print(isp(11))
+
